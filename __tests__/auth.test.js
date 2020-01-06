93a848219f1dae8074120f79aa966ac7abeb1c71
Zak Hyslin <rapscalan@gmail.com>
2020-01-05 22:20:54 -0800

    auth route passing

---
+++ __tests__/auth.test.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+
+describe('app routes', () => {
+    beforeAll(() => {
+        connect();
+    });
+
+    beforeEach(() => {
+        return mongoose.connection.dropDatabase();
+    });
+
+    afterAll(() => {
+        return mongoose.connection.close();
+    });
+
+    it('can signup a user with email and password', () => {
+        return request(app)
+            .post('/api/v1/auth/signup')
+            .send({ email: 'test@test.com', password: 'password' })
+            .then(res => {
+                expect(res.body).toEqual({
+                    _id: expect.any(String),
+                    email: 'test@test.com',
+                    __v: 0
+                });
+            });
+    });
+});
